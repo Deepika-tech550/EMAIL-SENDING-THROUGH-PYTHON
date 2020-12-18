59965f3dd346f3f8ff6f7b6b8825d0a1f4f078d3
Deepika-tech550 <75797675+Deepika-tech550@users.noreply.github.com>
2020-12-18 15:38:27 +0530

    Update sending _mail _through_ python.py

--- sending _mail _through_ python.py
+++ sending _mail _through_ python.py
@@ -1,15 +1,21 @@
 import emails
 
+def mail(gmail,name,reciept_no):
+    
+    subject="Hi "+name+" your receipt no. is"+reciept_no
 
+    message = emails.html(html="<p>Hi!<br>Here is your receipt...",
+                              subject=subject,
+                              mail_from=('DEEPIKA', 'pqr@gmail.com'))
 
-message = emails.html(html="<p>Hi!<br>Here is your receipt...",
-                          subject="your receipt no. is 23456",
-                          mail_from=('DEEPIKA', 'pqr@gmail.com'))
+    r = message.send(to='xyz@gmail.com', smtp={'host': 'smtp.gmail.com', 'timeout': 5,
+                                                      'port':587,
+                                                      'user':'abc@gmail.com',
+                                                      'password':'1234',
+                                                      'tls':True})
 
-r = message.send(to='xyz@gmail.com', smtp={'host': 'smtp.gmail.com', 'timeout': 5,
-                                                  'port':587,
-                                                  'user':'abc@gmail.com',
-                                                  'password':'1234',
-                                                  'tls':True})
-
-print(r.status_code)
+    print(r.status_code)
+g=input("Enter gmail")
+n=input("Enter name")
+rn=input("Enter reciept no")
+mail(g,n,rn)
